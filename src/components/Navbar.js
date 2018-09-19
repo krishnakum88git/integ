1fc42a9bad1a66547105afddce57ef9d3ce861fc
btbright <btbright@gmail.com>
2018-09-19 18:40:51 -0500

    very basic structure and started on admin structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,27 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from "react";
+import { Link } from "gatsby";
+import logo from "../img/logo.png";
 
 const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
+  <nav>
+    <div>
+      <Link to="/">
+        <img
+          src={logo}
+          alt="Integra Managed Care - Home"
+        />
+      </Link>
+    </div>
+    <div>
+      <Link to="/new-to-medicare">New to Medicare</Link>
+      <Link to="/plans">Our Plans</Link>
+      <Link to="/member-resources">Member Resources</Link>
+      <Link to="/providers">Providers</Link>
+      <Link to="/about-us">About Us</Link>
+      <Link to="/compliance">Compliance</Link>
+      <Link to="/contact-us">Contact Us</Link>
     </div>
   </nav>
-)
+);
 
-export default Navbar
+export default Navbar;
