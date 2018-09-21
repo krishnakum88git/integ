5d2031ebcc35effadc748220a63cac06360eea45
btbright <btbright@gmail.com>
2018-09-21 18:15:52 -0500

    added magnets to homepage

--- src/components/Magnet.js
+++ src/components/Magnet.js
@@ -2,13 +2,37 @@ import React from "react";
 import { Link } from "gatsby";
 import styled from "styled-components";
 import Img from "gatsby-image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  position: relative;
+`;
+const Dot = styled.div`
+  height: 32px;
+  width: 32px;
+  border-radius: 32px;
+  background-color: #ffffff;
+  position: relative;
+  ::before {
+    content: ' ';
+    left: 50%;
+    top: 50%;
+    margin: -8px 0 0 -8px;
+    position: absolute;
+    height: 16px;
+    width: 16px;
+    border-radius: 16px;
+    background-color: ${props => props.color || "#000000"}
+  }
+`;
 
-const Magnet = ({ linkPath, imgFixed }) => (
-  <Link to={linkPath}>
+const Magnet = ({ banner, slug, indicatorColor, iconName, title }) => (
+  <Link to={`/${slug}`}>
     <Wrapper>
-      <Img fixed={imgFixed} />
+      <Img fixed={banner.childImageSharp.fixed} />
+      <h3>{title}</h3>
+      <Dot color={indicatorColor} />
+      <FontAwesomeIcon style={{color: "rgba(255,255,255,0.9)"}} icon={iconName} />
     </Wrapper>
   </Link>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
+import Magnet from "../components/Magnet";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Img from "gatsby-image"
+import Img from "gatsby-image";
 
 export default class IndexPage extends React.Component {
   render() {
@@ -12,6 +13,11 @@ export default class IndexPage extends React.Component {
         <section>
           <FontAwesomeIcon style={{ color: "red" }} icon="heart" />
           <Img fluid={this.props.data.file.childImageSharp.fluid} />
+        </section>
+        <section>
+          {this.props.data.allMarkdownRemark.edges.map(({ node }) => (
+            <Magnet key={node.id} {...node.frontmatter} />
+          ))}
         </section>
       </Layout>
     );
@@ -44,9 +50,16 @@ export const pageQuery = graphql`
           id
           frontmatter {
             title
-            templateKey
+            slug
             iconName
             indicatorColor
+            banner {
+              childImageSharp {
+                fixed(width: 300) {
+                  ...GatsbyImageSharpFixed
+                }
+              }
+            }
           }
         }
       }
