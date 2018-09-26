f3d84603ab8f2baaf83a78a436214db9ef9e5b48
Scott Dodge <wsdodge@gmail.com>
2018-09-26 10:43:52 -0500

    tweak Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,54 +1,42 @@
-import React, { Fragment } from "react";
+import React from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Img from "gatsby-image";
+import DividerCurve from './DividerCurve'
 
 import { font } from "../styles/typography";
 
-const HeroSection = styled.section`
+const Hero = styled.div`
+  display: flex;
+  margin-bottom: 28px;
   position: relative;
-  height: 636px;
-  width: 100%;
-  display: flex;
-  flex-shrink: 0;
-  align-items: flex-end;
   justify-content: center;
+  flex-direction: column;
   overflow: hidden;
-`;
+`
 
-const HeroWrapper = styled.div`
-  height: 8000px;
-  width: 8000px;
-  border-radius: 8000px;
-  overflow: hidden;
-  display: flex;
-  align-items: flex-end;
-  justify-content: center;
-  flex-shrink: 0;
-  z-index: 1;
-  position: relative;
-`;
-
-const IconWrapper = styled.div`
-  background-color: #ffffff;
-  height: 128px;
-  width: 128px;
-  border-radius: 128px;
-  position: absolute;
-  top: 658px;
-  left: 50%;
-  margin-left: -64px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 99;
-`;
+const Image = ({browserWidth, image}) => image ? (
+  <Img
+    style={{
+      width: browserWidth,
+      height: '100%',
+      position: "absolute",
+      top: 0,
+      zIndex: -1
+    }}
+    fluid={image} />
+) : null
 
 const Icon = styled(FontAwesomeIcon)`
-  && {
-    width: 48px;
-    height: 64px;
-  }
+  position: absolute;  
+  padding: 32px;
+  width: 48px;
+  min-width: 48px;
+  max-width: 48px;
+  height: 48px;
+  background-color: #fff;
+  border-radius: 50%;
+  bottom: -28px;
 `;
 
 const GradientOverlay = styled.div`
@@ -59,13 +47,20 @@ const GradientOverlay = styled.div`
   left: 0;
   background: linear-gradient(
     to right,
-    rgba(0, 0, 0, 0.5) ${props => props.firstStop}px,
-    rgba(0, 0, 0, 0) ${props => props.secondStop}px
+    rgba(0, 0, 0, 0.5),
+    rgba(0, 0, 0, 0)
   );
-  z-index: 1;
 `;
 
-const HeroTitle = styled.h2`
+const Container = styled.div`
+  box-sizing: border-box;
+  margin: 0 auto;
+  padding: 175px 50% 175px 0;
+  width: 1200px;
+  z-index: 2;
+`
+
+const Title = styled.h2`
   ${font} font-size: 64px;
   color: #ffffff;
   text-shadow: 0px 2px 5px rgba(0, 0, 0, 0.5);
@@ -74,7 +69,7 @@ const HeroTitle = styled.h2`
   line-height: 64px;
 `;
 
-const HeroSubtitle = styled.h3`
+const Subtitle = styled.h3`
   ${font} font-size: 22px;
   color: #ffffff;
   text-shadow: 0px 2px 5px rgba(0, 0, 0, 0.5);
@@ -82,34 +77,26 @@ const HeroSubtitle = styled.h3`
   margin: 10px 0 0 0;
 `;
 
-const HeroCopy = styled.div`
-  position: absolute;
-  left: 50%;
-  top: 200px;
+const Footer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   z-index: 2;
-  max-width: 480px;
-  margin-left: -550px;
-`;
+`
 
 export default ({ browserWidth, iconColor, iconName, image, title, subTitle }) => (
-  <Fragment>
-    <HeroSection>
-      <HeroCopy>
-        <HeroTitle>{title}</HeroTitle>
-        <HeroSubtitle>
-          {subTitle}
-        </HeroSubtitle>
-      </HeroCopy>
-      <HeroWrapper>
-        <GradientOverlay
-          firstStop={4000 - browserWidth / 2}
-          secondStop={4000 + browserWidth / 2}
-        />
-        {image && <Img style={{ width: browserWidth, height: 636 }} fluid={image} />}
-      </HeroWrapper>
-    </HeroSection>
-    <IconWrapper>
+  <Hero>
+    <Image browserWidth={browserWidth} image={image} />
+    <GradientOverlay />
+    <Container>
+      <Title>{title}</Title>
+      <Subtitle>{subTitle}</Subtitle>
+    </Container>
+    <Footer>
+      <DividerCurve />
       <Icon style={{ color: iconColor }} icon={iconName} />
-    </IconWrapper>
-  </Fragment>
+    </Footer>
+  </Hero>
 );
