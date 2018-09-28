d2cfef9ce3f0b7c8887902337197e0a69fa1e240
btbright <btbright@gmail.com>
2018-09-28 17:18:01 -0500

    catch all external anchor clicks

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,36 +1,20 @@
 import React, { Component } from "react";
 import { Link as GatsbyLink } from "gatsby";
-import { Consumer } from "../store/createContext";
 
-class ExternalLink extends Component {
-  handleClick = cb => e => {
-    e.preventDefault();
-    cb(this.props.to);
-  };
-  render() {
-    return (
-      <Consumer>
-        {({ openExternalLinkModal }) => {
-          return (
-            <a
-              onClick={this.handleClick(openExternalLinkModal)}
-              href={this.props.to}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {this.props.children}
-            </a>
-          );
-        }}
-      </Consumer>
-    );
-  }
-}
+const ExternalLink = ({ children, to }) => (
+  <a href={to} target="_blank" rel="noopener noreferrer">
+    {children}
+  </a>
+);
 
 class Link extends Component {
   render() {
     const LinkComponent = this.props.to[0] === "/" ? GatsbyLink : ExternalLink;
-    return <LinkComponent {...this.props}>{this.props.title || this.props.children}</LinkComponent>;
+    return (
+      <LinkComponent {...this.props}>
+        {this.props.title || this.props.children}
+      </LinkComponent>
+    );
   }
 }
 
