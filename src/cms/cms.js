21d187638a51e4c00aeb3a19b6a42b3cb57d546f
Scott Dodge <wsdodge@gmail.com>
2018-12-19 17:31:41 -0600

    add cms plans preview and edit

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,12 +3,14 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import * as svgIcons from "@fortawesome/free-solid-svg-icons"
 import icons from '../styles/icons'
 import GenericPagePreview from './preview-templates/GenericPagePreview'
+import PlanPagePreview from './preview-templates/PlanPagePreview'
 import InfoPreview from './preview-templates/InfoPreview'
 import CmsInfoPreview from './preview-templates/CmsInfoPreview'
 
 icons.forEach(icon => library.add(svgIcons[icon]))
 
 CMS.registerPreviewTemplate('pages', GenericPagePreview)
+CMS.registerPreviewTemplate('plans', PlanPagePreview)
 CMS.registerPreviewTemplate('enrollmentInfo', InfoPreview)
 CMS.registerPreviewTemplate('contactInfo', InfoPreview)
 CMS.registerPreviewTemplate('cmsInfo', CmsInfoPreview)
---
+++ src/cms/preview-templates/PlanPagePreview.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { PageTemplate } from '../../templates/plan-page'
+import StyleInjector from '../../components/StyleInjector'
+import { Wrapper } from '../../components/Layout'
+import Hero from '../../components/Hero'
+import Callout from '../../components/Callout'
+import Breadcrumbs from '../../components/Breadcrumbs'
+import Footer from '../../components/Footer'
+
+const PlanPagePreview = ({ entry, widgetFor, widgetsFor, getAsset }) => {
+  const introduction = entry.getIn(['data', 'introduction'])
+  const breadcrumbs = entry.getIn(['data', 'breadcrumbs'])
+  const lists = entry.getIn(['data', 'lists'])
+
+  return (
+    <StyleInjector>
+      <Wrapper>
+        <Hero
+          title= {entry.getIn(['data', 'title'])}
+          subTitle= {entry.getIn(['data', 'subTitle'])}
+          imgSrc= {getAsset(entry.getIn(['data', 'banner']))}
+          iconColor= {entry.getIn(['data', 'indicatorColor'])}
+          iconName= {entry.getIn(['data', 'iconName'])}
+          isLarge= {entry.getIn(['data', 'isLarge'])}
+        />
+        {introduction && <Callout flex={false} {...introduction.toJS()} />}
+        {breadcrumbs && <Breadcrumbs items={breadcrumbs} />}
+        <PageTemplate
+          content={widgetFor('body')}
+          lists={lists && lists.toJS()}
+          listDirection={entry.getIn(['data', 'listDirection'])}
+        />
+        {/* {this.props.magnets && <Magnets magnets={this.props.magnets} />} */}
+        <div style={{height: 200}} />
+        <Footer disclaimers={widgetsFor('disclaimers')} />
+      </Wrapper>
+    </StyleInjector>
+  )
+}
+
+PlanPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
+
+export default PlanPagePreview
